f0ec0f372b36181a7b8de3fb92738e8bf06642f9
roman <roman121998yarkov@gmail.com>
2025-10-17 11:09:03 +0400

    hw номера

--- index.php
+++ index.php
@@ -138,5 +138,26 @@ if (isset($_SESSION['username'])): ?>
         </div>
     </div>
 <?php endif; ?>
+
+        <?php 
+    class GovNumber{
+        static function getNumber($str){
+            $match=array();         
+            $regex='/[АВЕКМНОРСТУХABEKMHOPCTYX]{1}[0-9]{3}[АВЕКМНОРСТУХABEKMHOPCTYX]{2}([0-9]{2,3})?/ui';
+         // echo $regex;
+            preg_match_all($regex,$str,$match);
+           
+           // print_r($str);
+            echo "Найденые номера: <br>";
+            foreach($match[0] as $item){
+                echo "Номер: " . $item . "<br>";
+            }
+            return $match[0];
+            }
+        }
+         GovNumber::getNumber("автомобиль с госномером В695СХ допустил столкновение с автомобилем марки Toyota corolla c госномером Т495СХ22");
+       //print_r($tmp);
+      //  echo $tmp;
+        ?>
 </body>
 </html>
